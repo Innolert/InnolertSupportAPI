117d8def59aa725fc1b7ce897f3e785a344d3ac9
olsynt <oleg2807@gmail.com>
2016-04-03 17:01:52 +0300

    beautify

--- server/api/appEvent/appEvent.model.js
+++ server/api/appEvent/appEvent.model.js
@@ -3,11 +3,17 @@
 import mongoose from 'mongoose';
 
 var AppEventSchema = new mongoose.Schema({
-  active: {type : Boolean , default : true},
-  createdAt: { type: Date, default: Date.now },
-  appName : String,
-  appVersion : String,
-  deviceId : String
+  active: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  appName: String,
+  appVersion: String,
+  deviceId: String
 });
 
 export default mongoose.model('AppEvent', AppEventSchema);
