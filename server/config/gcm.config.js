8f50396d4b918e8384533f2583c3cb5d036e4a33
olsynt <oleg2807@gmail.com>
2016-04-05 15:14:48 +0300

    clientGCM created. Added sender api and gcm

--- server/config/gcm.config.js
+++ server/config/gcm.config.js
@@ -1,18 +1,14 @@
+'use strict';
+
 var gcm = require('node-gcm');
 
-// Set up the sender with API key
-var sender = new gcm.Sender('AIzaSyD4MzVr4aNTI9Sn51lO1kU6UiCuMLkUWio');
-
-var message = new gcm.Message();
-
-message.addData('key1', 'msg1');
-
-var regTokens = ['YOUR_REG_TOKEN_HERE'];
-
+class clientGCM {
+  constructor(){
+    this.gcm = gcm;
+    //set up API key
+    this.sender = new gcm.Sender('AIzaSyD4MzVr4aNTI9Sn51lO1kU6UiCuMLkUWio');
+  }
+}
 
 
-// Now the sender can be used to send messages
-sender.send(message, { registrationTokens: regTokens }, function (err, response) {
-	if(err) console.error(err);
-	else 	console.log(response);
-});
+module.exports = clientGCM
