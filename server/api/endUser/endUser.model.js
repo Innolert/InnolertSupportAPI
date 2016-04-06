e5e16e5324a6ba2326664b459f80357f534a7f14
olsynt <oleg2807@gmail.com>
2016-04-06 15:16:13 +0300

    model has been modified

--- server/api/endUser/endUser.model.js
+++ server/api/endUser/endUser.model.js
@@ -3,9 +3,17 @@
 import mongoose from 'mongoose';
 
 var EndUserSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  active: {
+    type: Boolean,
+    default: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  email : String,
+  tokens : [mongoose.Schema.Types.Mixed],
+  mobileNumber : String
 });
 
 export default mongoose.model('EndUser', EndUserSchema);
