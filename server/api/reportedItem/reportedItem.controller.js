76f56a7cc2ff15acc79a5dfca6952642fe849604
olsynt <oleg2807@gmail.com>
2016-04-10 12:51:49 +0300

    minor changes

--- server/api/reportedItem/reportedItem.controller.js
+++ server/api/reportedItem/reportedItem.controller.js
@@ -76,8 +76,9 @@ export function show(req, res) {
 
 // Creates a new ReportedItem in the DB
 export function create(req) {
-  console.log(req);
-  return ReportedItem.create(req)
+    return ReportedItem.create(req.body)
+    .then(respondWithResult(res, 201))
+    .catch(handleError(res));
 }
 
 // Updates an existing ReportedItem in the DB
