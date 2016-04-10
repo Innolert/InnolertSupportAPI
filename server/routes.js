d075a63254b2718bd505ceff4f58620e0ba0de10
Oleg Sucharevich <oleg2807@gmail.com>
2016-04-10 15:46:47 +0300

    Issue_8
    Added new route to serve static files

--- server/routes.js
+++ server/routes.js
@@ -9,18 +9,14 @@ import path from 'path';
 export default function(app) {
   // Insert routes below
 
-  app.use('/api/uploadItems', require('./api/uploadItem')); 
+  app.use('/api/uploadItems', require('./api/uploadItem'));
   app.use('/api/endUsers', require('./api/endUser'));
   app.use('/api/reportedItems', require('./api/reportedItem'));
   app.use('/api/appEvents', require('./api/appEvent'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/orders', require('./api/order'));
   app.use('/auth', require('./auth').default);
-  app.use('/uploads/images', function(req,res){
-    res.sendFile(req._parsedOriginalUrl.path , { root: __base });
-  });    
-
-  // All undefined asset or api routes should return a 404
+  app.use(require("express").static('public'));  
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
