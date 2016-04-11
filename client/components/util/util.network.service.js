99c0692227bcba62f4e71464662d3b5cae0f0716
olsynt <oleg2807@gmail.com>
2016-04-11 17:44:15 +0300

    Issue_15   * Added new service that will provide all the request to the api

---
+++ client/components/util/util.network.service.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function() {
+
+/**
+ * This service is encapsulate all the rest requet to the server
+ */
+function networkService($http) {
+  var Util = {
+    /**
+     * GET     /api/GIVIN_URL
+     * POST    /api/GIVIN_URL
+     * GET     /api/GIVIN_URL/GIVIN_ID
+     * PUT     /api/GIVIN_URL/GIVIN_ID
+     * DELETE  /api/GIVIN_URL/GIVIN_ID
+     */
+      baseUrl : "http://localhost:9000/api/",
+      POST : function(){
+        return "abs";
+      },
+      GET : function(endPoint){
+        return $http.get(this.baseUrl+endPoint)
+      },
+      PUL : {},
+      DELETE : {},
+      UPDATE : {},
+      SHOW : {}
+  };
+
+  return Util;
+}
+
+angular.module('innolertApiApp.util')
+  .factory('networkService', networkService);
+
+})();
