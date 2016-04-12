80b53c12442358d0a728c5399d54bd5b6b8b6a2e
olsynt <oleg2807@gmail.com>
2016-04-12 12:00:09 +0300

    Issue_15:   * Minor changes

--- client/components/device.module/appContainer/deviceApp.js
+++ client/components/device.module/appContainer/deviceApp.js
@@ -1,79 +1,81 @@
 'use strict';
-(function(){
+(function() {
 
-class DeviceComponent {
-  constructor(networkService,$uibModal,socket) {
-    var ctrl = this;
-    ctrl.socket = socket;
-    ctrl.networkService = networkService;
-    ctrl.$uibModal = $uibModal;
-    ctrl.model = {};
-    networkService.GET("endUsers")
-    .then((response) => {
-      this.model.deviceList = response.data
-      this.socket.syncUpdates('endUser', this.model.deviceList);
-    })
+  class DeviceComponent {
+    constructor(networkService, $uibModal, socket) {
+      var ctrl = this;
+      ctrl.socket = socket;
+      ctrl.networkService = networkService;
+      ctrl.$uibModal = $uibModal;
+      ctrl.model = {};
+    }
 
-  }
-  updateList(data){
+    $onInit() {
+      this.networkService.GET("endUsers")
+        .then((response) => {
+          this.model.deviceList = response.data
+          this.socket.syncUpdates('endUser', this.model.deviceList);
+        })
+    }
+    updateList(data) {
       return this.model.deviceList
-  };
+    };
 
-  toggleResitration(){
-    var networkService = this.networkService;
-    var registrationModelInstance = this.$uibModal.open({
-      // template: '<device-resitration></device-resitration>',
-      templateUrl: 'components/device.module/deviceRegistration/deviceRegistrationTmpl.html',
-      size: 'md',
-      controllerAs : 'vm',
-      controller:function($uibModalInstance){
-        var ctrl = this;
-        ctrl.$uibModalInstance = $uibModalInstance;
-        ctrl.model = {
-          newDevice : {
-            name: null,
-            telNumber: null,
-            brand: null
-          },
-          supportedDevices : [{
+    toggleResitration() {
+      var networkService = this.networkService;
+      var registrationModelInstance = this.$uibModal.open({
+        // template: '<device-resitration></device-resitration>',
+        templateUrl: 'components/device.module/deviceRegistration/deviceRegistrationTmpl.html',
+        size: 'md',
+        controllerAs: 'vm',
+        controller: function($uibModalInstance) {
+          var ctrl = this;
+          ctrl.$uibModalInstance = $uibModalInstance;
+          ctrl.model = {
+            newDevice: {
+              name: null,
+              telNumber: null,
+              brand: null
+            },
+            supportedDevices: [{
               value: "Samsung",
               representation: "Samsung"
-          }, {
+            }, {
               value: "LG",
               representation: "LG"
-          }, {
+            }, {
               value: "Nexus",
               representation: "Nexus"
-          }, {
+            }, {
               value: "Iphone",
               representation: "Iphone"
-          }]
-        }
-        ctrl.save = function () {
-          this.$uibModalInstance.close(ctrl.model.newDevice);
-        };
+            }]
+          }
+          ctrl.save = function() {
+            this.$uibModalInstance.close(ctrl.model.newDevice);
+          };
 
-        ctrl.cancel = function () {
-          this.$uibModalInstance.dismiss(null);
-        };
+          ctrl.cancel = function() {
+            this.$uibModalInstance.dismiss(null);
+          };
         }
 
-    });
-    registrationModelInstance.result.then((data) => {
-      console.log("the return data from the model is :" , data);
-      networkService.POST('endUsers')
-      .then((response) => {
-        console.log(response);
+      });
+      registrationModelInstance.result.then((data) => {
+        console.log("the return data from the model is :", data);
+        networkService.POST('endUsers')
+          .then((response) => {
+            console.log(response);
+          })
       })
-    })
+    }
   }
-}
 
-angular.module('innolertApiApp.device')
-  .component('deviceAppContainer', {
-    templateUrl: 'components/device.module/appContainer/deviceAppTmpl.html',
-    controller: DeviceComponent,
-    controllerAs: "vm"
-  });
+  angular.module('innolertApiApp.device')
+    .component('deviceAppContainer', {
+      templateUrl: 'components/device.module/appContainer/deviceAppTmpl.html',
+      controller: DeviceComponent,
+      controllerAs: "vm"
+    });
 
 })();
