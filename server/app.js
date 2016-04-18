161f8de7d899f3fc3804fb8de790c6fb250d0a5f
olsynt <oleg2807@gmail.com>
2016-04-18 10:51:09 +0300

    Issue_26: test ssl

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ if (config.env !== 'production') {
 else{
   var server = require('https').createServer({
     key: fs.readFileSync('../innolert.key', 'utf8'),
-    cert: fs.readFileSync('../innolert.crt', 'utf8'),
+    cert: fs.readFileSync('../innolert_com.crt', 'utf8'),
     passphrase:  fs.readFileSync('../passphrase', 'utf8')
   }, app);
 }
