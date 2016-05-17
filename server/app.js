a01402fc400b9ebad4099eef060b25da0a938fdf
olsynt <oleg2807@gmail.com>
2016-05-17 14:35:35 +0300

    https://github.com/Innolert/InnolertAPI/issues/53

--- server/app.js
+++ server/app.js
@@ -46,7 +46,7 @@ else{
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
   })
-  .listen(443);
+  .listen(config.port, config.ip);
 
 }
 var socketio = require('socket.io')(server, {
