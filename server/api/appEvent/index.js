b5e504991a607a5da8c12a2c724502085a780e51
olsynt <oleg2807@gmail.com>
2016-06-02 16:56:16 +0300

    https://github.com/Innolert/InnolertAPI/issues/64

--- server/api/appEvent/index.js
+++ server/api/appEvent/index.js
@@ -6,7 +6,7 @@ var controller = require('./appEvent.controller');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.get('/:id', auth.hasRole('user'), controller.show);
+router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', auth.hasRole('admin'), controller.update);
