1d9fb8eee45fc2bcbf766f19f227ac541f408580
olsynt <oleg2807@gmail.com>
2016-06-08 18:50:48 +0300

    https://github.com/Innolert/InnolertAPI/issues/76

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -9,10 +9,8 @@ var router = new Router();
 router.get('/verify/:id', controller.verify);
 router.get('/', auth.hasRole('admin'), controller.index);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
-// router.get('/me', auth.isAuthenticated(), controller.me);
-// router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
-router.put('/:id/fcm', auth.isAuthenticated(), controller.updateFCMToken);
+router.get('/me', auth.isAuthenticated(), controller.me);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', controller.create);
-
+router.put('/:id', auth.isAuthenticated(), controller.changePassword);
 module.exports = router;
