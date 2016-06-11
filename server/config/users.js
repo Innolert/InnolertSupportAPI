c6804634cb3f801f509a305c06fa16713b28147d
Oleg Sucharevich <Oleg2807@gmail.com>
2016-06-11 21:39:50 +0300

    https://github.com/Innolert/InnolertAPI/issues/82 	* Added some comments to users.js 	* Probably should be renamed

--- server/config/users.js
+++ server/config/users.js
@@ -1,3 +1,5 @@
+// This object will save global socketio connection that was init in app.js as socketio
+// And each new client that connects will have entry here with array to all the reference of open sessions
 module.exports = {
 	
 }
