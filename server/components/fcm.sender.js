97d626610cbc4c69414dbf676439db087f243d56
olsynt <oleg2807@gmail.com>
2016-06-13 16:44:21 +0300

    https://github.com/Innolert/InnolertAPI/issues/80

--- server/components/fcm.sender.js
+++ server/components/fcm.sender.js
@@ -4,7 +4,24 @@ var FCM = require('fcm-node');
 var serverKey = JSON.parse(fs.readFileSync('../apis.key.json', 'utf8')).fcm;
 var fcm = new FCM(serverKey)
 
-export default function(message){
+export function sendWithMessage(message){
+  send(message);
+}
+
+//data shlold be an object
+export function sendWithRegistrationIdAndData(registrationId,data){
+  if( !data instanceof Object)
+    throw "Data must be an object" ;
+  //TODO - oleg : if the registrationId is an array we should throw an error
+  var message = {
+      to: registrationId,
+      data: data
+  };
+  send(message)
+}
+
+function send(message){
+  console.log("Sending message using fcm" , message);
   fcm.send(message, function(err, response){
       if (err) {
           console.log("Something has gone wrong!" , err);
