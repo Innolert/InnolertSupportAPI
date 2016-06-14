b4811c314480db33b5bf6918aca1d7e500b802ff
olsynt <oleg2807@gmail.com>
2016-06-14 16:09:21 +0300

    https://github.com/Innolert/InnolertAPI/issues/85   * Added bl to the server

--- server/api/order/order.controller.js
+++ server/api/order/order.controller.js
@@ -179,6 +179,18 @@ function updateUserDeviceState(device, message) {
     },
     reset_password: () => {
       device.state.deviceLocked.isEventPassedToDevice = true;
+    },
+    wfoN: () => {
+      device.state.wifi.isEventPassedToDevice = true;
+    },
+    wfoff: () => {
+      device.state.wifi.isEventPassedToDevice = true;
+    },
+    bton: () => {
+      device.state.bluetooth.isEventPassedToDevice = true;
+    },
+    btoff: () => {
+      device.state.bluetooth.isEventPassedToDevice = true;
     }
   }
   if (cases[message]) {
@@ -199,19 +211,35 @@ function deviceIsAbleToGetOperation(device, message) {
     },
     start_voice_record: () => {
       return !device.state.audioRecorded.isEventPassedToDevice ||
-        !device.state.audioRecorded.isAudioRecording
+        !device.state.audioRecorded.isAudioRecording;
     },
     stop_voice_record: () => {
       return !device.state.audioRecorded.isEventPassedToDevice ||
-        device.state.audioRecorded.isAudioRecording
+        device.state.audioRecorded.isAudioRecording;
     },
     lock_device: () => {
       return !device.state.deviceLocked.isDeviceLocked &&
-        !device.state.deviceLocked.isEventPassedToDevice
+        !device.state.deviceLocked.isEventPassedToDevice;
     },
     reset_password: () => {
       return device.state.deviceLocked.isDeviceLocked &&
-        !device.state.deviceLocked.isEventPassedToDevice
+        !device.state.deviceLocked.isEventPassedToDevice;
+    },
+    wfoN: () => {
+      return !device.state.wifi.isWifiOn &&
+             !device.state.wifi.isEventPassedToDevice;
+    },
+    wfoff: () => {
+      return device.state.wifi.isWifiOn &&
+            !device.state.wifi.isEventPassedToDevice;
+    },
+    bton: () => {
+      return !device.state.wifi.isWifiOn &&
+             !device.state.wifi.isEventPassedToDevice;
+    },
+    btoff: () => {
+      return  device.state.wifi.isWifiOn &&
+             !device.state.wifi.isEventPassedToDevice;
     }
   }
   if (cases[message]) {
