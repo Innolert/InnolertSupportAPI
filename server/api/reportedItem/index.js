faadb024ad63af55d9961a7b8d11df50384ea9b2
olsynt <oleg2807@gmail.com>
2016-06-15 11:13:10 +0300

    https://github.com/Innolert/InnolertAPI/issues/82   * Added auth libs

--- server/api/reportedItem/index.js
+++ server/api/reportedItem/index.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var controller = require('./reportedItem.controller');
+import * as auth from '../../auth/auth.service';
 
 var router = express.Router();
-// Oleg : temporary only admin have an access 
+// Oleg : temporary only admin have an access
 router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/:id', auth.hasRole('admin'), controller.show);
 router.post('/', auth.hasRole('admin'), controller.create);
