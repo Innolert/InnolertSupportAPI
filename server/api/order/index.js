0273270abc715eafc4b381e9e4ddcbd3e7c91cff
olsynt <oleg2807@gmail.com>
2016-06-15 12:33:31 +0300

    https://github.com/Innolert/InnolertAPI/issues/82

--- server/api/order/index.js
+++ server/api/order/index.js
@@ -6,7 +6,7 @@ var controller = require('./order.controller');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.get('/:id', auth.hasRole('user'), controller.show); // Oleg : send self id and type=<SOCIAL_NETWORK> to get data about it
+router.get('/:id', controller.show); // Oleg : send self id and type=<SOCIAL_NETWORK> to get data about it
 router.post('/',auth.hasRole('user'), controller.create); // Oleg :all the orders to will be passed from this endpoint
 router.put('/:id', auth.hasRole('admin'), controller.update);
 router.patch('/:id', auth.hasRole('admin'), controller.update);
