4e8a0910daaac867c7b145de6014c9a90bfb464e
olsynt <oleg2807@gmail.com>
2016-06-19 14:04:59 +0300

    https://github.com/Innolert/InnolertAPI/issues/92

--- server/api/endUser/endUser.socket.js
+++ server/api/endUser/endUser.socket.js
@@ -26,7 +26,7 @@ export function register(socket) {
 function createListener(event, socket) {
   return function(doc) {
     //Before emitting the event we've to check that the client is able to see doc
-    if (socketioConnections[doc.parentUser] && socketioConnections[doc.parentUser].indexOf(socket.client.id) != -1){
+    if (socketioConnections[doc.parentUser]){
       console.log("Sending event to ", doc.parentUser);
       socket.emit(event, doc);
       fcm.sendToUserIdEndUserUpdates(doc.parentUser,doc);
