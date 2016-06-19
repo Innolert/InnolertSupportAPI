0f6cc5c1d4f16273ed01f8486db7aa4d1ed95208
olsynt <oleg2807@gmail.com>
2016-06-19 14:13:57 +0300

    https://github.com/Innolert/InnolertAPI/issues/92

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -222,9 +222,10 @@ export function removeUnregisteredTokenFromUser(userId, deviceIndex){
   console.log("Attempt to remove token from" , userId);
   findById(userId)
   .then(user => {
-  console.log("found user" , user);
-   if(user)
-    delete user.devices[deviceIndex].privateTokens.fcm;
-    user.save();
+    console.log("found user" , user);
+    if(user){
+      delete user.devices[deviceIndex].privateTokens.fcm;
+      user.save();
+    }
   })
 }
