723952da4dce1435b88fdb4ddd94ef7077881d2f
olsynt <oleg2807@gmail.com>
2016-06-19 14:01:53 +0300

    https://github.com/Innolert/InnolertAPI/issues/92

--- server/components/fcm.sender.js
+++ server/components/fcm.sender.js
@@ -30,7 +30,7 @@ export function sendToUserIdAppEventUpdates(){
 }
 
 function send(message, userId, deviceIndex){
-  console.log("Sending message using fcm" , message);
+  console.log("Sending message using fcm to" , message.to);
   fcm.send(message, function(err, response){
       if (err) {
           console.log("Something has gone wrong!" , err);
