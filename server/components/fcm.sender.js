c47ea48fbda72b9e407c247905daf87fe8c7843e
olsynt <oleg2807@gmail.com>
2016-06-22 16:30:45 +0300

    https://github.com/Innolert/InnolertAPI/issues/92   * Now it is done

--- server/components/fcm.sender.js
+++ server/components/fcm.sender.js
@@ -22,7 +22,7 @@ export function sendToUserIdEndUserUpdates(userId, endUserId, event){
             endUserId: endUserId
           }
         };
-        send(message, handleErrorFcm(user._id, deviceIndex));
+        send(message, handleErrorFcm(user._id, index));
       })
   })
 }
@@ -40,7 +40,7 @@ export function sendToUserIdAppEventUpdates(userId, endUserId, docToSend, event)
           endUserId: endUserId
         }
       }
-      send(message, handleErrorFcm(user._id, deviceIndex));
+      send(message, handleErrorFcm(user._id, index));
     })
   })
 }
@@ -66,7 +66,7 @@ function send(message, done){
 /**
  * [handleErrorFcm description]
  * @param  {stinrg} err [the error that returns from fcm-push]
- * @return {function} that 
+ * @return {function} that
  */
 function handleErrorFcm(userId,deviceIndex){
   return function(err){
